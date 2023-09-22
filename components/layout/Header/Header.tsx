e4e5aa1a20056704eec86456522f889627807ffe
ialmendrasafe <almendraivan210814@gmail.com>
2023-09-22 13:49:24 -0300

    configuracion sin i18n

---
+++ components/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { useTranslation } from "react-i18next";
+
+export default async function Header() {
+  const [t] = useTranslation("global");
+  return (
+    <header className="text-sm text-neutral-500 dark:text-neutral-400">
+      {t(["header.product"])}
+    </header>
+  );
+}
